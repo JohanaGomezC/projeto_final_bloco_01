d541024cd065ab157cc5210c05ea86763c247f3f
Johana Gomez <johagomez9806@gmail.com>
2026-05-07 10:40:20 -0300

    Commit Menu

---
+++ src/ecommerce/Menu.java
@@ -0,0 +1,65 @@
+package ecommerce;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao;
+		
+		while (true) {
+	
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                MODA - ECOMMERCE                     ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Cadastrar Produto                    ");
+			System.out.println("            2 - Listar todos os produtos             ");
+			System.out.println("            3 - Atualizar Dados do produto           ");
+			System.out.println("            4 - Deletar produto                      ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			
+			opcao = leia.nextInt();
+			
+			if (opcao == 0) {
+				System.out.println("\n MODA ECOMMERCE - OBRIGADO POR NOS PREFERIR");
+				sobre();
+			    leia.close();
+				System.exit(0);
+			}
+			
+			switch (opcao) {
+			case 1:
+				System.out.println( "Cadastrando produto\n\n");
+				break;
+			case 2:
+				System.out.println( "Listar todos os produtos\n\n");
+				break;
+			case 3:
+				System.out.println("Atualizar dados do produto\n\n");
+				break;
+			case 4:
+				System.out.println( "Deletar produro\n\n");
+				break;
+			default: 
+					System.out.println("\nOpção Inválida");
+			}
+		}
+	}
+
+	public static void sobre() {
+		System.out.println("*****************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Johana Gómez Cardona - johagomez9806@gmail.com");
+		System.out.println("https://github.com/JohanaGomezC");
+		System.out.println("*****************************************************");
+	}
+
+}
